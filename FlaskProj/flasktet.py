2f9932b589382fd481046b0dff1113dafa2f321a
waqar.muhammad@slu.edu <waqar.muhammad@slu.edu>
2018-09-19 22:59:11 -0500

    updated code fixed db bugs, removed unwanted files from dir, changed data stroage structure a bit, refactored code

--- FlaskProj/flasktet.py
+++ FlaskProj/flasktet.py
@@ -3,86 +3,62 @@ import os
 from flask import Flask, jsonify, request, json
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
+from Processing_engine import Processing_engine
+from CouchAPI import CouchAPI
+
 
 app = Flask(__name__)
+
 CORS(app)
+
 api = Api(app)
 
-def GetDictionaryFiles(data):
-    print data
-    return {"data":["Waqar", "Muhammad"]}
+global couchOBJ
+global engineOBJ
 
-class dummy(Resource):
+
+engineOBJ = Processing_engine()
+couchOBJ = CouchAPI('Administrator', 'password', 'localhost')
+
+
+class AuthenticateUser(Resource):
+
     def post(self):
-        data1 = json.loads(request.data.decode())
-        print data1
-        lang_names = []
-        with open('lang_names.txt', 'rb') as f:
-            lang_names = f.readlines()
-        lang_names = [x.split('\t')[1].replace('\n','') for x in lang_names if data1['lang'] in x.split('\t')[0]]
-        if len(lang_names) > 0:
-            lang_names = lang_names[0]
 
-        print lang_names
+        request_data = json.loads(request.data.decode())
+        engine = globals()['engineOBJ']
+        return engine.verify_user(request_data)
 
-        with open(lang_names+'.dic', 'rb') as f:
-            dic_data = f.readlines()
 
-        with open(lang_names+'.aff', 'rb') as f:
-            affix_data = f.readlines()
+class GetCurrentUser(Resource):
 
-        return {'dic': dic_data, 'affix': affix_data}
-
-class GetLangNames(Resource):
     def post(self):
-        with open('lang_names.txt', 'rb') as f:
-            data =  f.readlines()
-        data = [x.split('\t')[0] for x in data]
-
-        data.remove('\n')
-
-        return data
+        return globals()['user']
 
 
 class SaveData(Resource):
+
     def post(self):
 
-        data = json.loads(request.data.decode())
+        request_data = json.loads(request.data.decode())
+        engine = globals()['engineOBJ']
+        return engine.save_data(request_data)
 
-        lang_name = data['lang_name']
-        lang_symb = data['lang_symbol']
-        aff_data = data['aff_data']
-        dic_data = data['dic_data']
+class RetrieveLangNames(Resource):
 
-        file_names = []
-        with open('lang_names.txt', 'rb') as f:
-            file_names = f.readlines()
-        file_names = [x.split('\t')[0] for x in file_names]
+    def post(self):
 
-        if lang_name in file_names:
-            return {'Error': 'File name already exists please provide different file name'}
-
-        with open('lang_names.txt', 'a+') as f:
-            f.write('\n'+lang_name+'\t'+lang_symb)
-
-        dic_file_name = lang_symb+'.dic'
-        aff_file_name = lang_name+'.aff'
-        if os.path.exists(dic_file_name):
-            return {'Error': 'Dictionary file already exists with name "'+dic_file_name+'" please provide different name'}
-
-        if os.path.exists(aff_file_name):
-            return {'Error': 'Dictionary file already exists with name "'+aff_file_name+'" please provide different name'}
-
-        with open(dic_file_name, 'wb') as f:
-            f.writelines(dic_data)
-
-        with open(aff_file_name, 'wb') as f:
-            f.writelines(aff_data)
+        engine = globals()['engineOBJ']
+        return engine.retrieve_lang_names()
 
 
-api.add_resource(dummy,'/dummy')
-api.add_resource(SaveData,'/SaveData')
-api.add_resource(GetLangNames,'/langs')
+
+
+
+
+api.add_resource(RetrieveLangNames,'/retrieve_langs')
+api.add_resource(AuthenticateUser,'/auth')
+api.add_resource(SaveData,'/save_data')
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1',threaded = True)

9af806b63a88bf3fb5adac9d3178b067b1c88bf4
waqarmuhammad1 <waqar.muhammad@slu.edu>
2018-10-11 17:56:16 -0500

    Added DB Setter Methods in User.py

--- FlaskProj/User.py
+++ FlaskProj/User.py
@@ -6,10 +6,11 @@ class User():
         self.paradigm_name = None
         self.lang_name = None
         self.user_data = None
+        self.lang_data = None
+
         self.word = 'Word'
         self.skeleton = 'Skeleton'
         self.couch = CouchAPI('Administrator', 'password', 'localhost')
-        self.couch.open_bucket('data')
         pass
 
 
@@ -36,18 +37,24 @@ class User():
         if result == True:
             self.username = username
             self.user_data = self.couch.retrieve_data(self.username).value
+            self.couch.open_bucket('data')
         else:
             return {'Error': 'User not found'}
 
     ################################################################    GETTERS     ############################################################
+
+
     def get_user_languages(self):
         return  self.user_data.keys()
 
     def get_user_paradigms(self, lang_name):
         self.lang_name = lang_name
+        self.lang_data = self.user_data[lang_name]
         pass
 
-    def get_user_paradigm_words(self):
+    def get_user_paradigm_words(self, paradigm_name):
+        self.paradigm_name = paradigm_name
+        self.paradigm_data = self.lang_data[paradigm_name]
         pass
 
     def get_user_paradigm_words_data(self):
@@ -58,15 +65,49 @@ class User():
 
 
     ################################################################    SETTERS     ############################################################
-    def set_user_language(self):
 
-        pass
+    #Input Params lang_name -> string:English   |    DB Structure -> admin:{English: {}}
+    def set_user_language(self, lang_name):
 
-    def set_user_paradigm(self):
-        pass
+        if lang_name in self.user_data:
+            return {'Error': 'Language already exists'}
 
-    def set_user_paradigm_words(self):
-        pass
+        self.user_data[lang_name] = {}
+        self.save_data()
 
-    def set_user_paradigm_words_data(self):
-        pass
+
+    #Input Params   paradigm_name -> string:Noun       ,   paradigm_skeleton -> list:[root, singular, plural]  |    DB Structure -> admin:{English: {Noun: {Word: {}, Skeleton: [root, singular, plural]}}}
+    def set_user_paradigm(self, paradigm_name, paradigm_skeleton):
+
+        if paradigm_name in self.lang_data:
+            return {'Error': 'Paradigm name already exists'}
+
+        self.user_data[self.lang_name][paradigm_name] = {'Words': {}, 'Skeleton': paradigm_skeleton}
+        self.save_data()
+
+    # Input Params   paradigm_name -> string:Noun       ,   paradigm_skeleton -> list:[root, singular, plural]  |    DB Structure -> admin:{English: {Noun: {Word: {run: [runner, running, runs]},
+    #                                                                                                                                       Skeleton: [root, singular, plural]}}}
+    def set_user_paradigm_words(self, root_word, word_forms):
+
+        if root_word in self.paradigm_data:
+            return {'Error': 'Word already exits'}
+
+        self.user_data[self.lang_name][self.paradigm_name][self.word][root_word] = word_forms
+        self.save_data()
+
+    def save_data(self):
+        self.couch.store_data(self.username, self.user_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+

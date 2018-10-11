84bde6b19c46d3eb96c9eda7fb07d759d8fa39a1
waqarmuhammad1 <waqar.muhammad@slu.edu>
2018-10-11 17:07:09 -0500

    New user class

--- FlaskProj/DataHolder.py
+++
@@ -1,9 +0,0 @@
-from Processing_engine import Processing_engine
-from CouchAPI import CouchAPI
-
-class DataHolder():
-
-    def __init__(self):
-        self.engine_obj = Processing_engine()
-        self.couch_obj = CouchAPI('Administrator', 'password', 'localhost')
-
